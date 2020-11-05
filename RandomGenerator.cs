2d04ee32523c0dc3ff6d25d4bb241ac97d59f740
Dillon O'Brien <58051128+DillonOBrien@users.noreply.github.com>
2020-11-05 14:59:22 -0700

    Script for Generating Numbers

---
+++ RandomGenerator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceNumber : Behaviour {
+
+	Vector3 diceVelocity
+
+	void FixedUpdate () {
+		diceVelocity = DiceScript.diceVelocity;
+	}
+
+void OnTriggerStay(Collier col)
+{
+	if (diceV.x == 0f && diceV.y == 0f && diceVelocity.z == 0f)
+	{
+		switch (col.gameObject.name) {
+		case "Side1":
+			DiceNumberDisplay.diceNumber = 1;
+			break;
+		case "Side2":
+			DiceNumberDisplay.diceNumber = 2;
+			break;
+		case "Side3":
+			DiceNumberDisplay.diceNumber = 3;
+			break;
+		case "Side4":
+			DiceNumberDisplay.diceNumber = 4;
+			break;
+		case "Side5":
+			DiceNumberDisplay.diceNumber = 5;
+			break;
+		case "Side6":
+			DiceNumberDisplay.diceNumber = 6;
+			break;
+		}
+	}
+}
+}
